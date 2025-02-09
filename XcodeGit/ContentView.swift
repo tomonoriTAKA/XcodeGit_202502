5e2d1345ec39d42ae480234d724719614e75e5ff
Tomonori <tomonori0204.takahashi@gmail.com>
2025-02-09 13:18:50 +0900

    2回目のコミットです

--- XcodeGit/ContentView.swift
+++ XcodeGit/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("初めてのコミット")
+            // 2回目のコミット
         }
         .padding()
     }
