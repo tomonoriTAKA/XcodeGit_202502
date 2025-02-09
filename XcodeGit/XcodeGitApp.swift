1d9e9d851fbfee17f3d23d229ee435ce592d4d62
Tomonori <tomonori0204.takahashi@gmail.com>
2025-02-09 12:01:26 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by 高橋知憲 on 2025/02/09.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
